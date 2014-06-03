aeffeb5f782f9a511cc1837f6260dcd5acc1da6e
ISHII Tsuubito <ishii@buffer-renaiss.com>
2014-06-03 18:58:02 +0900

    add method converts from Plain String to String as brainfuck source

--- Brainfuck.swift
+++ Brainfuck.swift
@@ -10,6 +10,16 @@ import Foundation
 protocol InterpretBrainfuck {
     func stringAtPosition(pos:Int) -> NSString;
     func interpretAsBrainfuck();
+    func toBrainfuckSource() -> NSString;
+}
+
+operator infix ** {}
+@infix func ** (str:String, n:Int) -> String {
+    var s = "";
+    for i in 1...n {
+        s += str;
+    }
+    return s;
 }
 
 extension NSString: InterpretBrainfuck {
@@ -65,5 +75,43 @@ extension NSString: InterpretBrainfuck {
             }
             cursor++;
         }
+    }
+    
+    func toBrainfuckSource() -> NSString {
+        var source: String = ("+" ** 8) + "[>" + ("+" ** 8) + "<-]>+[>"
+                           + (">+>" ** length)
+                           + ("<<" ** length)
+                           + "<-]";
+        for(var i:Int = 0; i < self.length; i++) {
+            var char:Int = Int(characterAtIndex(i)) - 65;
+            var sign = char < 0 ? -1 : 1;
+            char *= sign;
+            var (m, n, r) = calculateThreePair(char);
+            var sourcePiece = createSourcePieces(m, n: n, r: r, op: sign < 0 ? "-" : "+");
+            source += ">" + sourcePiece;
+        }
+        
+        return source;
+    }
+    
+    func createSourcePieces(m:Int, n:Int, r:Int, op:String) -> String {
+        return ("+" ** m) + "[>" + (op ** n) + "<-]>" + (op ** r) + ".";
+    }
+    
+    func calculateThreePair(num:Int) -> (Int, Int, Int) {
+        func sumPairs(p:(Int, Int, Int)) -> Int {
+            var (x, y, z) = p;
+            return x + y + z;
+        }
+        var sqrtNum = Int(sqrtf(CFloat(num)));
+        var pairs:(Int, Int, Int) = (num, num, num);
+        for i in 1...sqrtNum {
+            for j in 1...sqrtNum {
+                if sumPairs((i, j, num - i * j)) < sumPairs(pairs) {
+                    pairs = (i, j, num - i * j);
+                }
+            }
+        }
+        return pairs;
     }
 }
